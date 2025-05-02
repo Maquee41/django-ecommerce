d34a86c2b5f7f79a689a8bf2195d016d42c26a17
Egor <egor.goliov2006@inbox.ru>
2025-05-03 01:18:17 +0300

    feat(catalog): add routing for product categries

--- voltzone/catalog/urls.py
+++ voltzone/catalog/urls.py
@@ -5,6 +5,7 @@ import catalog.views
 
 urlpatterns = [
     path("", catalog.views.product_list, name="product-list"),
+    path('category/<slug:category_slug>/', catalog.views.product_categories, name='product_list_by_category'),
     re_path(
         r"^(?P<product_id>\d+)/$",
         catalog.views.product_detail,
--- voltzone/catalog/views.py
+++ voltzone/catalog/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import get_object_or_404, render
 
-from catalog.models import Product
+from catalog.models import Category, Product
 
 
 def product_list(request):
     products = Product.objects.all()
     context = {"products": products}
+    return render(request, "catalog/product_list.html", context)
+
+
+def product_categories(request, category_slug):
+    category = get_object_or_404(Category, slug=category_slug)
+    products = Product.objects.filter(category=category).order_by("price")
+    context = {
+        "category": category,
+        "products": products,
+    }
     return render(request, "catalog/product_list.html", context)
 
 
